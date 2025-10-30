5022d6513b813a93751493f2e86751800d34a419
JonathanYogev <tantanyogev@gmail.com>
2025-10-30 14:03:04 +0200

    try pr

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Argo CD! This is our Dev environment.');
+  res.send('Hello, Argo CD!!! This is our Dev environment.');
 });
 
 app.listen(port, () => {
